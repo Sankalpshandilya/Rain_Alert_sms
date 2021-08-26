80bf0462efdda4cd614d71f2a894a427cd3635d8
SANKALP SHANDILYA <sankalp.bwp.shandilya@gmail.com>
2021-08-26 17:29:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#Note! For the code to work you need to replace all the placeholders with
+#Your own details. e.g. account_sid, lat/lon, from/to phone numbers.
+
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "602135b30cf7b7bf34bc1c4a6fab58af"
+account_sid = "AC8215cb2263ea7b9d1210c8701ef2674d"
+auth_token = "8e0c98ee48bc893e67e77660fceb80ab"
+
+weather_params = {
+    "lat": "26.356060",
+    "lon": "86.363632",
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="+17405665952",
+        to="+919334971039"
+    )
+    print(message.status)
